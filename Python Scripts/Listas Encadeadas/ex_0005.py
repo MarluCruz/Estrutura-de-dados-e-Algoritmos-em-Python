d52ce878aab3926bf9eb269c62fb8c26ea9a6e94
MarluCruz <65717006+MarluCruz@users.noreply.github.com>
2022-11-13 21:58:47 -0300

    ...

---
+++ Python Scripts/Listas Encadeadas/ex_0005.py
@@ -0,0 +1,61 @@
+#Lista Duplamente Encadeada
+class No:
+    def __init__(self, valor):
+        self.valor = valor
+        self.proximo = None
+        self.anterior = None
+    def mostrar_no(self):
+        print(self.valor)
+class ListaDuplamenteEncadeada:
+    def __init__(self):
+        self.primeiro = None
+        self.ultimo = None
+    def __lista_vazia(self):
+        return self.primeiro == None
+    def insere_inicio(self, valor):
+        novo = No(valor)
+        if self.__lista_vazia():
+            self.ultimo = novo
+        else:
+            self.primeiro.anterior = novo
+        novo.proximo = self.primeiro
+        self.primeiro = novo
+    def insere_final(self, valor):
+        novo = No(valor)
+        if self.__lista_vazia():
+            self.primeiro = novo
+        else:
+            self.ultimo.proximo = novo
+            novo.anterior = self.ultima
+        self.ultimo = novo
+    def excluir_inicial(self):
+        temp = self.primeiro
+        if self.primeiro.proximo == None:
+          self.ultimo = None
+        else:
+    def e
+            self.primeiro.proximo.anterior = None  
+        self.primeiro = self.primeiro.proximo
+        return temp
+    def mostrar_frente(self):
+        atual = self.primeiro
+        print("____________")
+        while atual != None:
+            atual.mostrar_no()
+            atual = atual.proximo
+        print("____________")
+    def mostrar_tras(self):
+        atual = self.ultimo
+        print("____________")
+        while atual != None:
+            atual.mostrar_no()
+            atual = atual.anterior
+        print("____________")
+lista = ListaDuplamenteEncadeada()
+lista.insere_inicio(1)
+lista.insere_inicio(2)
+lista.insere_inicio(3)
+lista.insere_inicio(4)
+lista.insere_inicio(5)
+lista.mostrar_frente()
+lista.mostrar_tras()
