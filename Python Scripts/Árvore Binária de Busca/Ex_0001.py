0faf949266d72b677c45e33015528cf299cf9f24
MarluCruz <65717006+MarluCruz@users.noreply.github.com>
2022-11-19 22:01:25 -0300

    ....

--- Python Scripts/Árvore Binária de Busca/Ex_0001.py
+++ Python Scripts/Árvore Binária de Busca/Ex_0001.py
@@ -43,6 +43,51 @@ class ArvoreBinarariaBusca:
             if atual == None:
                 return None
         return atual
+        # Raiz, esquerda, direita
+    def pre_ordem(self, no):
+        if no != None:
+            print(no.valor)
+            self.pre_ordem(no.esquerda)
+            self.pre_ordem(no.direita)
+    # Esquerda, raiz, direita
+    def em_ordem(self, no):
+        if no != None:
+            self.em_ordem(no.esquerda)
+            print(no.valor)
+            self.em_ordem(no.direita)
+    def no_pos_ordem(self, no):
+        if no != None:
+            self.no_pos_ordem(no.esquerda)
+            self.no_pos_ordem(no.direita)
+            print(no.valor)
+    def excluir(self, valor):
+        if self.raiz == None:
+            print('A arvore está vazia')
+            return
+        #Encontrar o nó
+        atual = self.raiz
+        pai = self.raiz
+        e_esquerda = True
+        while atual.valor != valor:
+            pai = atual
+            #Esquerda
+            if valor < atual.valor:
+                e_esquerda = True
+                atual = atual.esquerda
+            #Direita
+            if valor > atual.valor:
+                e_esquerda = False
+                atual = atual.direita
+            if atual == None:
+                return None
+        # O nó a ser apagado é uma folha
+        if (atual.esquerda == None) and (atual.direita == None):
+            if atual == self.raiz:
+                self.raiz == None
+            elif e_esquerda == True:
+                pai.esquerda = None
+            else:
+                pai.direita = None   
 #Inserção e visualização:
 arvore = ArvoreBinarariaBusca()
 arvore.inserir(53)
@@ -65,3 +110,8 @@ if arvore.pesquisar(72) == None:
     (print('Elemento não localizado'))
 else:
     print('Elemento localizado')
+
+#Travessia
+arvore.pre_ordem(arvore.raiz)
+arvore.em_ordem(arvore.raiz)
+arvore.no_pos_ordem(arvore.raiz)
